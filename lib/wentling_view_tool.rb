008ac181fd68193723ac963681355e9764fb85dc
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 14:47:06 +0000

    Dev tool initial commit

---
+++ lib/wentling_view_tool.rb
@@ -0,0 +1,6 @@
+require "wentling_view_tool/version"
+require "wentling_view_tool/renderer"
+
+module WentlingViewTool
+  # Your code goes here...
+end
---
+++ lib/wentling_view_tool/version.rb
@@ -0,0 +1,3 @@
+module WentlingViewTool
+  VERSION = "0.1.0"
+end
